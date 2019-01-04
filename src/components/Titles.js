e29fd9ce77f18c24e60cc04478dfb45d0857d082
maayanrosenthal <maayan.rose1@gmail.com>
2019-01-04 18:08:58 +0200

    Cleaning up
    Neater files

--- src/components/Titles.js
+++ src/components/Titles.js
@@ -1,15 +1,13 @@
 import React from "react";
-import App from "../App";
+//import App from "../App";
 
-class Titles extends React.Component {
-    render () {
-        return (
-            <div>
-                <h1>Weather Finder</h1>
-                <p>Find out temperature, conditions and more. </p>
-            </div>
-        );
-    }
-};
+const Titles = () => {
+    return (
+        <div>
+            <h1>Weather Finder</h1>
+            <p>Find out temperature, conditions and more. </p>
+        </div>
+    );
+    };
 
 export default Titles;
