88699abc1c7d4aead6435b648caa7c4205d5e691
maayanrosenthal <maayan.rose1@gmail.com>
2019-01-04 18:00:01 +0200

    weather app to learn react
    woohoo

--- src/App.js
+++ src/App.js
@@ -1,12 +1,54 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+import Titles from "./components/Titles";
+import Form from "./components/Form";
+import Weather from "./components/Weather";
+//import { conditionalExpression } from 'babel-types';
+
+const API_KEY = "a16926f80505ad5980bd32982d72c658";
+
 class App extends Component {
+  state = {
+    temperature: undefined,
+    city: undefined,
+    country: undefined,
+    humidity: undefined,
+    description: undefined,
+    error: undefined
+  };
+  getWeather = async(e) => {
+    const city = e.target.elements.city.value;
+    const country = e.target.elements.country.value;
+    e.preventDefault();
+    const api_call = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=metric`);
+    const data = await api_call.json();
+    console.log(data);
+    if(city && country){
+      this.setState({
+        temperature: data.main.temp,
+        city: data.name,
+        country: data.sys.country,
+        humidity: data.main.humidity,
+        description: data.weather[0].description,
+        error: ""
+      });
+    }
+    else{
+      this.setState({
+        temperature: undefined,
+        city: undefined,
+        country: undefined,
+        humidity: undefined,
+        description: undefined,
+        error: "Please enter the values."
+      });
+      }
+  }
   render() {
-    return (
+    return ( // You can only include one tag to export within return...
       <div className="App">
-        <header className="App-header">
+        {/* <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
             Edit <code>src/App.js</code> and save to reload.
@@ -19,10 +61,20 @@ class App extends Component {
           >
             Learn React
           </a>
-        </header>
+        </header> */}
+        <Titles/> 
+        <Form getWeather={this.getWeather}/>
+        <Weather 
+        temperature={this.state.temperature}
+        city={this.state.city}
+        country={this.state.country}
+        humidity={this.state.humidity}
+        description={this.state.description}
+        error={this.state.error}
+        />
       </div>
     );
   }
 }
-
-export default App;
+ 
+export default App; //export means making this component available to other factors. 
